0fb2b14f8de59a0dc025cf56e0481b12fcfe3e58
WestleyArgentum <WestleyArgentum@gmail.com>
2015-09-04 19:03:55 -0400

    clean up initialization

--- event-system/event-system.js
+++ event-system/event-system.js
@@ -6,17 +6,13 @@ var events = require('events'),
 // all loaded modules
 var puppyEmitter = new events.EventEmitter();
 
-module_files = fs.readdirSync('./modules');
-module_files = module_files.filter(function (filename) {
+module_files = fs.readdirSync('./modules').filter(function (filename) {
     return filename.indexOf("-module") > -1;
 });
 
-module_fns = module_files.map(function (filename) {
-    return require('./modules/' + filename);
-});
-
-modules = module_fns.map(function (fn) {
-    return new fn(puppyEmitter);
+modules = module_files.map(function (filename) {
+    var Module = require('./modules/' + filename);
+    return new Module(puppyEmitter);
 });
 
 puppyEmitter.emit('newTweet', 'beep bloop bop, I am a computer!');
