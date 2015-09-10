522c61e1950090f1436a455601ee805e2967ce8a
WestleyArgentum <WestleyArgentum@gmail.com>
2015-09-09 21:06:51 -0400

    refactor tweet so that it doesn't require a cb

--- event-system/modules/twitter.js
+++ event-system/modules/twitter.js
@@ -5,13 +5,15 @@ var TwitterModule = module.exports = function (eventEmitter, config) {
     this.twit = new Twit(config);
 };
 
-TwitterModule.prototype.tweet = function (status, cb) {
-    if (!cb) {
-        throw new Error('you must pass a callback to the tweet function');
-    } else if (typeof status !== 'string') {
-        return cb(new Error('tweet must be of type String'));
+TwitterModule.prototype.tweet = function (status) {
+    if (typeof status !== 'string') {
+        throw new Error('tweet must be of type String');
     } else if (status.length > 140) {
-        return cb(new Error('tweet is too long: ' + status.length));
+        throw new Error('tweet is too long: ' + status.length);
     }
-    this.twit.post('statuses/update', { status: status }, cb);
-};
+    this.twit.post('statuses/update', { status: status }, function (err) {
+        if (err) {
+            throw err;
+        }
+    });
+}
