96d6f700bb46fda1628e8da4d196b40aa2aa526d
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2015-10-28 16:18:38 -0700

    it's just an emitter

--- event-system/event-system.js
+++ event-system/event-system.js
@@ -5,7 +5,7 @@ var events = require('events'),
 
 // global event system used for communication between
 // all loaded modules
-var puppyEmitter = new events.EventEmitter();
+var emitter = new events.EventEmitter();
 
 // load and initialize all of the modules
 var modules = {};
@@ -13,7 +13,7 @@ for (var i = 0; i < config['modules'].length; ++i) {
     var moduleConfig = config['modules'][i];
 
     var Module = require(moduleConfig['file']);
-    modules[moduleConfig['moduleName']] = new Module(puppyEmitter, moduleConfig['config']);
+    modules[moduleConfig['moduleName']] = new Module(emitter, moduleConfig['config']);
 }
 
 // set up the connections between modules
@@ -23,5 +23,5 @@ for (var i = 0; i < config['connections'].length; ++i) {
     var module = modules[connection['module']],
         fn = module[connection['fn']];
 
-    puppyEmitter.on(connection['on'], fn.bind(module));
+    emitter.on(connection['on'], fn.bind(module));
 }
